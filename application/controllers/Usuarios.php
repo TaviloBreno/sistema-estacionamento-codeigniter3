8ff2e56d65caa7073aa82d5a2a2599451993d41a
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-16 16:58:25 -0300

    Listando usuários do banco de dados

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Usuarios extends CI_Controller
+{
+	public function index()
+	{
+		$data = array(
+			'titulo' => 'Usuários Cadastrados',
+			'usuarios' => $this->ion_auth->users()->result(),
+		);
+
+		$this->load->view('layout/header', $data);
+		$this->load->view('usuarios/index');
+		$this->load->view('layout/footer');
+	}
+}
