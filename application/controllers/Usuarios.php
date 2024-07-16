a93cd518ab9523c64d381a048c164ba6036b0a96
Tavilo Breno <breno_wk2@hotmail.com>
2024-07-16 17:03:30 -0300

    Dinamizando o carregamento de arquivos css e javascript

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -8,6 +8,15 @@ class Usuarios extends CI_Controller
 	{
 		$data = array(
 			'titulo' => 'Usuários Cadastrados',
+			'subtitulo' => 'Listando todos os usuários cadastrados no sistema',
+			'styles' => [
+				'plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+			],
+			'scripts' => [
+				'plugins/datatables.net/js/jquery.dataTables.min.js',
+				'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+				'plugins/datatables.net/js/estacionamento.js',
+			],
 			'usuarios' => $this->ion_auth->users()->result(),
 		);
 
